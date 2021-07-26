2ebfac68bbea019f6b6c0c557bd45246afe947e6
Dhees <mahdiyyatul@gmail.com>
2021-07-26 12:37:28 +0700

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  plugins: [],
+  plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: { 'prettier/prettier': ['error', { endOfLine: 'auto' }] },
 }
